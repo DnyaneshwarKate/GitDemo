5a567bb97ea9327c8583d8cc6e8d43cb6637d5d4
Dnyaneshwar Kate <katednyaneshwar223@gmail.com>
2022-12-24 18:33:44 +0530

    first commit : code of list

---
+++ src/com/demogit/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.demogit;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Amit");
+		list.add("Abhishek");
+		list.add("Pratik");
+		list.add("Rutik");
+
+		System.out.println(list);
+	}
+}
