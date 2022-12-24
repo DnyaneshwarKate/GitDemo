0c88af371ce912ce696c7fd6cd08c51e972be943
Dnyaneshwar Kate <katednyaneshwar223@gmail.com>
2022-12-24 18:39:52 +0530

    Second commit : code of linkedlist

---
+++ src/com/demogit/LinkedListDemo.java
@@ -0,0 +1,16 @@
+package com.demogit;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String > list = new LinkedList<String>();
+		list.add("Pavan");
+		list.add("Chetan");
+		list.add("Swapnil");
+		list.add("Rutik");
+		System.out.println(list);
+	}
+}
